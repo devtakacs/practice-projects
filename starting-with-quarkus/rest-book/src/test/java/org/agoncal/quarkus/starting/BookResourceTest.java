4c4dffd8d2d392136b70c15cc744e3e28402eff9
devtakacs <p.takacs.dev@gmail.com>
2026-05-07 11:20:16 +0200

    Testing Book REST endpoint

--- starting-with-quarkus/rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
+++ starting-with-quarkus/rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -1,7 +1,11 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
 import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpClient;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,12 +13,39 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 class BookResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void shouldGetAllBooks() {
         given()
-          .when().get("/api/books")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON).
+        when()
+            .get("/api/books").
+        then()
+            .statusCode(200)
+            .body("size()", is(2));
     }
 
+    @Test
+    void shouldCountAllBooks() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.TEXT_PLAIN).
+            when()
+                .get("/api/books/count").
+            then()
+                .statusCode(200)
+                .body(is("2"));
+    }
+
+    @Test
+    void shouldGetABook() {
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .pathParam("id", 1).
+            when()
+                .get("/api/books/{id}").
+            then()
+                .statusCode(200)
+                .body("title", is("Book 1"))
+                .body("author", is("Author 1"))
+                .body("yearOfPublication", is(2026))
+                .body("genre", is("Test genre 1"));;
+    }
 }
