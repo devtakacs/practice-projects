bcc33ef51380dfd0c9deecbc29d01ecea9d1da4d
devtakacs <p.takacs.dev@gmail.com>
2026-05-18 23:56:17 +0200

    Packaging the application

--- starting-with-quarkus/rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
+++ starting-with-quarkus/rest-book/src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
@@ -1,8 +1,8 @@
 package org.agoncal.quarkus.starting;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.NativeImageTest;
 
-@QuarkusIntegrationTest
+@NativeImageTest
 class BookResourceIT extends BookResourceTest {
     // Execute the same tests but in packaged mode.
 }
