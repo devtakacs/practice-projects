f5e8e77d523d12f121371d237e28c89bc1e6bc69
devtakacs <p.takacs.dev@gmail.com>
2026-06-12 01:37:14 +0200

    Developing the number microservice
    - exposing the number rest endpoint
    - customizing the json output
    - documenting the number microservice

---
+++ vintagestore/rest-number/src/main/java/org/agoncal/quarkus/microservices/number/NumberMicroservice.java
@@ -0,0 +1,26 @@
+package org.agoncal.quarkus.microservices.number;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+@ApplicationPath("/")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Number Microservice",
+                description = "This microservice generates book numbers",
+                version = "1.0",
+                contact = @Contact(name = "@myname", url = "localhost:8080")
+        ),
+        externalDocs = @ExternalDocumentation(url = "localhost:8080"),
+        tags = {
+                @Tag(name = "api", description = "Public API that can be used by anybody"),
+                @Tag(name = "numbers", description = "Anybody interested in numbers")
+        }
+)
+public class NumberMicroservice extends Application {
+}
--- vintagestore/rest-number/src/main/java/org/agoncal/quarkus/microservices/number/NumberResource.java
+++ vintagestore/rest-number/src/main/java/org/agoncal/quarkus/microservices/number/NumberResource.java
@@ -1,15 +1,19 @@
 package org.agoncal.quarkus.microservices.number;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import java.time.Instant;
 import java.util.Random;
-import javax.inject.Inject;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 @Path("/api/numbers")
+@Tag(name = "Number  REST Endpoint")
 public class NumberResource {
 
     @Inject
@@ -17,6 +21,10 @@ public class NumberResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary = "Generates book numbers",
+            description = "ISBN 13 and ISBN 10 numbers"
+    )
     public IsbnNumbers generateIsbnNumbers() {
         IsbnNumbers isbnNumbers = new IsbnNumbers();
         isbnNumbers.isbn13 = "13-" + new Random().nextInt(100_000_000);
