51fe2b20b6f027aa4604d2d59919fff41e63f742
devtakacs <p.takacs.dev@gmail.com>
2026-06-14 23:08:25 +0200

    Testing the number microservice

--- vintagestore/rest-number/src/test/java/org/agoncal/quarkus/microservices/number/NumberResourceTest.java
+++ vintagestore/rest-number/src/test/java/org/agoncal/quarkus/microservices/number/NumberResourceTest.java
@@ -4,7 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.hasKey;
 
 @QuarkusTest
 class NumberResourceTest {
@@ -13,8 +14,10 @@ class NumberResourceTest {
         given()
           .when().get("/api/numbers")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+                .statusCode(200)
+                .body("isbn_13", startsWith("13-"))
+                .body("isbn_10", startsWith("10-"))
+                .body(not(hasKey("generationDate")));
     }
 
 }
