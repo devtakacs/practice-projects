562b0830fd696a6590e196db66a488672122b864
devtakacs <p.takacs.dev@gmail.com>
2026-06-15 19:05:15 +0200

    Integrate Number service REST client
    Add MP-style REST client and wiring to fetch ISBNs from the Number microservice. Changes include:
    - Add NumberProxy interface (@RegisterRestClient) and IsbnThirteen DTO (maps isbn_13 JSON).
    - Add NumberProxyProducer that builds a Resteasy proxy and produces it for @RestClient injection.
    - Update BookResource to inject @RestClient NumberProxy and use it to populate book.isbn13.
    - Add resteasy-client dependency to rest-book/pom.xml and document rationale.
    - Configure the NumberProxy base URI in application.properties (http://localhost:8701).
    - Add IDE copilotDiffState.xml tracking pending diffs.
    These changes enable the book service to call the number service to generate ISBN-13 values at runtime.

--- vintagestore/rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
+++ vintagestore/rest-book/src/main/java/org/agoncal/quarkus/microservices/book/BookResource.java
@@ -1,11 +1,13 @@
 package org.agoncal.quarkus.microservices.book;
 
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+// ...existing code...
 import org.jboss.logging.Logger;
 
 import java.time.Instant;
@@ -13,6 +15,10 @@ import java.time.Instant;
 @Path("/api/books")
 @Tag(name="Book REST endpoint")
 public class BookResource {
+
+    @Inject
+    @RestClient
+    NumberProxy proxy;
 
     @Inject
     Logger logger;
@@ -26,7 +32,7 @@ public class BookResource {
     )
     public Response createABook(@FormParam("title") String title, @FormParam("author") String author, @FormParam("year") int yearOfPublication, @FormParam("author") String genre) {
         Book book = new Book();
-        book.isbn13 = "13-We will get it later from the Number micorservice";
+        book.isbn13 = proxy.generateIsbnNumbers().isbn13;
         book.title = title;
         book.author = author;
         book.yearOfPublication = yearOfPublication;
---
+++ vintagestore/rest-book/src/main/java/org/agoncal/quarkus/microservices/book/NumberProxyProducer.java
@@ -0,0 +1,25 @@
+package org.agoncal.quarkus.microservices.book;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+@ApplicationScoped
+public class NumberProxyProducer {
+
+    @ConfigProperty(name = "org.agoncal.quarkus.microservices.book.NumberProxy/mp-rest/uri")
+    String baseUri;
+
+    @Produces
+    @RestClient
+    public NumberProxy numberProxy() {
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target(baseUri);
+        return target.proxy(NumberProxy.class);
+    }
+}
+
